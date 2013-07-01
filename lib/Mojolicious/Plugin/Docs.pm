5803cf1169a589df64d3a98f5259081cce8e84d4
Anton Konovalov <antoni.konovalov@gmail.com>
2013-07-01 14:23:11 +0400

    generation plugin

---
+++ lib/Mojolicious/Plugin/Docs.pm
@@ -0,0 +1,44 @@
+package Mojolicious::Plugin::Docs;
+use Mojo::Base 'Mojolicious::Plugin';
+
+our $VERSION = '0.01';
+
+sub register {
+  my ($self, $app) = @_;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Mojolicious::Plugin::Docs - Mojolicious Plugin
+
+=head1 SYNOPSIS
+
+  # Mojolicious
+  $self->plugin('Docs');
+
+  # Mojolicious::Lite
+  plugin 'Docs';
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::Docs> is a L<Mojolicious> plugin.
+
+=head1 METHODS
+
+L<Mojolicious::Plugin::Docs> inherits all methods from
+L<Mojolicious::Plugin> and implements the following new ones.
+
+=head2 register
+
+  $plugin->register(Mojolicious->new);
+
+Register plugin in L<Mojolicious> application.
+
+=head1 SEE ALSO
+
+L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicio.us>.
+
+=cut
